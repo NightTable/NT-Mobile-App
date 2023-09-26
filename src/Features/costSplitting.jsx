a6bcf6603fcab3ad9337bfa74ed93eb6d358eb9d
amiyasekhar <amiyasekhar@gmail.com>
2023-09-26 01:45:38 -0400

    any new changes

--- src/Features/costSplitting.jsx
+++ src/Features/costSplitting.jsx
@@ -1,9 +1,3 @@
-// All information, source code contained in this document
-// is the property of StrynDev Solutions, LLC. It must not
-// be transmitted to others without the written consent of
-// StrynDev Solutions. It must be returned to StrynDev Solutions
-// when its authorized use is terminated.
-
 import React from "react";
 
 import {
@@ -69,54 +63,49 @@ const CostSplittingSectionComp = (props) => {
             {props.tableTypeSelection === "pnsl" ? (
               <Text
                 style={[
-                  typography.regular.regular14,
+                  typography.regular.regular16,
                   {
                     color: colors.gold.gold100,
                   },
                 ]}
               >
-                You have chosen the{" "}
+                By choosing the{" "}
                 <Text
                   style={[
-                    typography.regular.regular14,
+                    typography.regular.regular16,
                     {
                       color: colors.gold.gold100,
                     },
                   ]}
                 >
-                  pay-now-split-later{" "}
+                  "pay-now-split-later"{" "}
                 </Text>
-                method. This means that you are reserving a table and are
-                responsible for paying the full cost of the table initially upon
-                creation of the request.
+                method, you commit to paying full table minimum, plus additional fees, upon creating the table request, then split the cost amongst others later.
               </Text>
             ) : (
               <Text
                 style={[
-                  typography.regular.regular12,
+                  typography.regular.regular16,
                   {
                     color: colors.gold.gold100,
                   },
                 ]}
               >
-                You have chosen the{" "}
+                By choosing the{" "}
                 <Text
                   style={[
-                    typography.regular.regular12,
+                    typography.regular.regular16,
                     {
                       color: colors.gold.gold100,
                     },
                   ]}
                 >
-                  split-now-pay-later{" "}
+                  "split-now-pay-later"{" "}
                 </Text>
-                method. This means that you are choosing to assign each
-                participant a joining fee. Note that this method does not create
-                an official reservation upon creation of the request; it only
-                gives you the option to negotiate fees with participants before
-                finalizing anything. You may lose your table selections to
-                someone else who either chooses the pay-now-split-later method,
-                or finalizes their reservation before yours.
+                method, you can invite others and discuss individual joining fees while we temporarily hold your card.
+                Payments will be finalized once everyone agrees on their respective shares.
+                However, please note that the table is not reserved until the total payment is made.
+                This means there's a risk of losing the table to a group that pays in full upfront or settles faster than your group.
               </Text>
             )}
           </View>
@@ -129,7 +118,7 @@ const CostSplittingSectionComp = (props) => {
               props.tableTypeSelection === "pnsl" ? (
                 <Text
                   style={[
-                    typography.regular.regular12,
+                    typography.regular.regular16,
                     {
                       color: colors.gold.gold100,
                     },
@@ -150,15 +139,12 @@ const CostSplittingSectionComp = (props) => {
               ) : (
                 <Text
                   style={[
-                    typography.regular.regular14,
+                    typography.regular.regular16,
                     {
                       color: colors.gold.gold100,
                     },
                   ]}
                 >
-                  By selecting the create request button, you are aknowledge
-                  that you have not made an official reservation, but are asking
-                  people to join a potential table group.
                 </Text>
               )
               //                        Once everyone's joining fees have been finalised, you will be able to finalize your reservation.
@@ -172,7 +158,7 @@ const CostSplittingSectionComp = (props) => {
             {props.tableTypeSelection === "pnsl" ? (
               <Text
                 style={[
-                  typography.regular.regular12,
+                  typography.regular.regular16,
                   {
                     color: colors.gold.gold100,
                   },
@@ -185,19 +171,12 @@ const CostSplittingSectionComp = (props) => {
             ) : (
               <Text
                 style={[
-                  typography.regular.regular12,
+                  typography.regular.regular16,
                   {
                     color: colors.gold.gold100,
                   },
                 ]}
               >
-                Once everyone's joining fees have been finalized, you will be
-                able to finalize your reservation. If your table selections are
-                reserved by another group, neither you nor the participants in
-                this request will be charged, and the pending charges made to
-                participants' cards will not appear in their card statements.
-                Note that participants, once they join a table, cannot leave
-                unless the organizer or promoter cancels the request.
               </Text>
             )}
           </View>
